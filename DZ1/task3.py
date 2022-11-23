2616ac8c8d107cf0a38f3d301f9430d31076f03c
Yuri Malyshev <team21_07@list.ru>
2022-11-23 14:57:45 +0300

    task3 complete

---
+++ DZ1/task3.py
@@ -0,0 +1,7 @@
+'''
+Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
+Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+'''
+string_out = input('Введите число :')
+number_out = int(string_out) + int(string_out + string_out) + int(string_out + string_out + string_out)
+print(string_out, '+', string_out + string_out, '+', string_out + string_out + string_out, '=', number_out)
