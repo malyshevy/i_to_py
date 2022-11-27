010b7a60f7bfe7909d3aa5297cf7244ca13d624a
Yuri Malyshev <team21_07@list.ru>
2022-11-27 21:14:29 +0300

    task6 complete

--- DZ1/task6.py
+++ DZ1/task6.py
@@ -13,3 +13,15 @@
 6-й день: 3,22
 Ответ: на 6-й день спортсмен достиг результата — не менее 3 км.
 '''
+number_distance_fierst = float(input('Результат спортсмена в первый день (введите длинну дистанции пробежки в первый день) :'))
+number_distance_second = float(input('Введите желаемую длинну дистанции пробежки :'))
+number_i = 1
+if (number_distance_fierst > 0) & (number_distance_second > 0):
+    print(f'{number_i} день: {number_distance_fierst:.2f}')
+    while number_distance_fierst <= number_distance_second:
+        number_distance_fierst = number_distance_fierst + (number_distance_fierst * 0.1)
+        number_i += 1
+        print(f'{number_i} день: {number_distance_fierst:.2f}')
+else:
+    print('Введите значение отличное от "0"')
+print(f'Ответ: на {number_i} день спортсмен достиг результата - не менее {number_distance_second:.2f}')
