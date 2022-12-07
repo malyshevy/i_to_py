8ea568c642f27a3a2689ab2159051b54e4d04220
Yuri Malyshev <team21_07@list.ru>
2022-12-07 19:00:14 +0300

    task4 complete

---
+++ DZ4/task4.py
@@ -0,0 +1,26 @@
+'''
+Представлен список чисел. Определить элементы списка, не имеющие повторений.
+Сформировать итоговый массив чисел, соответствующих требованию.
+Элементы вывести в порядке их следования в исходном списке. Для выполнения задания обязательно использовать генератор.
+Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
+Результат: [23, 1, 3, 10, 4, 11]
+'''
+from random import randint
+
+list_generator = [randint(1, 15) for i in range(0, 15)]
+print(list_generator)
+new_list = []
+for i in list_generator:
+    j = 0
+    max = 0
+    while j < len(list_generator):
+        if i == list_generator[j]:
+            max += 1
+            j += 1
+        else:
+            j += 1
+    if max > 1:
+        continue
+    else:
+        new_list.append(i)
+print(new_list)
