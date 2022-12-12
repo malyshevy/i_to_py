d72389c0a359347945d6bdc070e20389a7891a63
Yuri Malyshev <team21_07@list.ru>
2022-12-12 17:40:39 +0300

    task3 & task3.txt complete

--- DZ5/task3.py
+++ DZ5/task3.py
@@ -6,7 +6,7 @@
 Иванов 23543.12
 Петров 13749.32
 '''
-with open('task3.txt', 'r') as f_3:
+with open('task3.txt', 'r', encoding="utf-8") as f_3:
     size = sum(1 for _ in f_3)
     f_3.seek(0)
     list_f_task3 = []
@@ -16,9 +16,17 @@ with open('task3.txt', 'r') as f_3:
         i += 1
     fio = None
     zp = None
+    summ = 0
+    summ_second = 0
+    j = 0
     for i in list_f_task3:
         i = i.split()
+        summ = summ + int(i[1])
         if int(i[1]) < 20000:
             zp = (int(i[1]))
             fio = (i[0])
+            summ_second = summ_second + zp
             print(f'{fio} : {zp}')
+            j = j + 1
+    print(f'Средняя величина дохода выбранных сотрудников : {float(summ_second / j)}')
+    print(f'Средняя величина дохода всех сотрудников : {float(summ / size)}')
