30025dcee8e296515840d0720f2b0070de0e0c9f
Yuri Malyshev <team21_07@list.ru>
2022-12-12 17:59:58 +0300

    task4 & task4.txt & task4_new.txt complete

--- DZ5/task4.py
+++ DZ5/task4.py
@@ -8,3 +8,15 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 '''
+zamena = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+list_zamena = []
+with open('task4.txt', 'r', encoding="utf-8") as task4:
+    for i in task4:
+        print(i)
+        i = i.split(' ', 1)
+        list_zamena.append(zamena[i[0]] + '  ' + i[1])
+with open('task4_new.txt', 'w+', encoding="utf-8") as task4_new:
+    task4_new.writelines(list_zamena)
+    task4_new.seek(0)
+    for i in task4_new:
+        print(i)
