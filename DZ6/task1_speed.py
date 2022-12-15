2a9946521b4e4520a065c535c81366ce1678c030
Yuri Malyshev <team21_07@list.ru>
2022-12-15 09:31:55 +0300

    task1_speed & time_speed complete

--- DZ6/task1_speed.py
+++ DZ6/task1_speed.py
@@ -46,3 +46,60 @@ def max_sekond():
 Скорость работы уменьшилась в 9.859760035836032 раз
 '''
 
+
+
+def rank_first():
+    # Функция возведения в степень
+    def my_func(x, y):
+        '''
+        :param x: первое введённое число
+        :param y: второе введённое число
+        :return: список ответов
+        '''
+        f = 1  # Переменная со значением 1
+        num_ferst = 0
+        num_second = f
+        num_fry = 0
+        if x > 0 and y < 0:
+            # Первый вариант решения
+            num_ferst = x ** y
+            # Второй вариант решения
+            i = 0
+            while i > y:
+                num_second = num_second * x
+                i -= f
+            num_second = f / num_second
+            # Третий вариант решения
+            i = f
+            num_fry = x
+            d = x
+            while i < abs(y):
+                j = f
+                while j < x:
+                    num_fry += d
+                    j += f
+                d = num_fry
+                i += f
+            num_fry = f / num_fry
+        else:
+            print('Введите верные числа...')
+            exit()
+        num_list = [num_ferst, num_second, num_fry]
+        return num_list
+
+    # Ввод данных
+    my_num_ferst = 12
+    # int(input('Введите действительное положительное число : '))
+    my_num_second = -3
+    # int(input('Введите целое отрицательное число : '))
+    # Вывод на экран
+    my_num_list = my_func(my_num_ferst, my_num_second)
+    return my_num_list
+    #print(f'Вариант 1 = {my_num_list[0]:.5f}, Вариант 2 = {my_num_list[1]:.5f}, Вариант 3 = {my_num_list[2]:.5f}')
+
+
+def rank_seсond():
+    my_num_ferst = 12
+    my_num_second = -3
+    return pow(my_num_ferst, my_num_second)
+
--- DZ6/time_speed.py
+++ DZ6/time_speed.py
@@ -1,15 +1,17 @@
 '''
-Замер времени выполнения 2 х функций из task1_speed
+Замер времени выполнения 4 х функций из task1_speed
 '''
 from timeit import timeit
 from task1_speed import max_first
 from task1_speed import max_sekond
+from task1_speed import rank_first
+from task1_speed import rank_seсond
 
 a = timeit("max_first()", globals=globals(), number=10000)
 b = timeit("max_sekond()", globals=globals(), number=10000)
 print(f'Скорость работы первого скрипта : {a}')
 print(f'Скорость работы оптимизированного скрипта : {b}')
-print(f"Скорость работы уменьшилась в {a / b} раз")
+print(f"Скорость работы уменьшилась в {a / b} раз \n")
 
 '''Скрипт оптимизировал применением встроенных функций max и list'''
 '''
@@ -17,3 +19,9 @@ print(f"Скорость работы уменьшилась в {a / b} раз")
 Скорость работы оптимизированного скрипта : 0.008902600035071373
 Скорость работы уменьшилась в 9.859760035836032 раз
 '''
+'''Скрипт оптимизировал применением встроенной функции pow'''
+c = timeit("rank_first()", globals=globals(), number=10000)
+d = timeit("rank_seсond", globals=globals(), number=10000)
+print(f'Скорость работы первого скрипта : {c}')
+print(f'Скорость работы оптимизированного скрипта : {d}')
+print(f"Скорость работы уменьшилась в {c / d} раз")
