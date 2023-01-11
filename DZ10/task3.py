7d3c48527862abe4cb5a9eadef77277d5cecc7a4
Yuri Malyshev <team21_07@list.ru>
2023-01-12 01:32:59 +0300

    task3 complete

--- DZ10/task3.py
+++ DZ10/task3.py
@@ -1,3 +1,29 @@
 '''
 Создать метакласс для паттерна Синглтон (см. конец вебинара)
 '''
+
+
+class MyMetaClass(type):
+    a = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.a:
+            cls.a[cls] = super(MyMetaClass, cls).__call__(*args, **kwargs)
+        return cls.a[cls]
+
+
+class MyClass(metaclass=MyMetaClass):
+    pass
+
+
+class TestClass():
+    pass
+
+
+a_obj = MyClass()
+b_obj = MyClass()
+print(a_obj == b_obj)
+
+a_obj = TestClass()
+b_obj = TestClass()
+print(a_obj == b_obj)
